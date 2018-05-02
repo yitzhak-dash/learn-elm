5343d5a3cc4b6ca686938847e41e8c4b0147485d
Yitzhak Dashevsky <idashevsky@gmail.com>
2018-05-02 12:53:33 +0300

    format basic example

--- basic-example.elm
+++ basic-example.elm
@@ -1,37 +1,53 @@
+module Main exposing (..)
+
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 
 
-main = Html.beginnerProgram { model = model, view = view, update = update }
+main =
+    Html.beginnerProgram { model = model, view = view, update = update }
+
 
 
 -- MODEL
 
-type alias Model = Int
+
+type alias Model =
+    Int
+
 
 model : Model
-model = 0
+model =
+    0
+
 
 
 -- UPDATE
 
-type Msg = Increment | Decrement
+
+type Msg
+    = Increment
+    | Decrement
+
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Increment ->
-      model + 2
-    Decrement ->
-      model - 2
+    case msg of
+        Increment ->
+            model + 2
+
+        Decrement ->
+            model - 2
+
 
 
 -- VIEW
 
+
 view : Model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (toString model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (toString model) ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
